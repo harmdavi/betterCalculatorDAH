51d720b681467349f42d2f40536f7a663ca70a17
harmdavi <harmondrum@gmail.com>
2020-09-06 15:14:30 -0600

    Add project files.

---
+++ My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("betterCalculatorDAH")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("betterCalculatorDAH")>
+<Assembly: AssemblyCopyright("Copyright ©  2020")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("ea8b9d39-a6db-4a6c-80e6-35f0870b4a3c")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")>
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
---
+++ betterCalculatorDAH.vb
@@ -0,0 +1,104 @@
+﻿Module betterCalculatorDAH
+
+    Sub Main()
+        Dim firstNumber As String
+        Dim work1 As Decimal
+        Dim work2 As Decimal
+        Dim secondNumber As String
+        Dim operation As String
+        Dim userInput As String
+        Dim badData As Boolean
+        Dim result As Decimal
+
+        'This is just a into screen. it will not appear again in the calculator 
+        Console.WriteLine("This is a basic Calculator. _ 
+if at any time you would like to quit the calculator, press q ")
+
+        userInput = Console.ReadLine
+        If userInput = "q" Then
+            Exit Sub
+
+        End If
+
+
+
+
+        Do
+
+
+
+
+
+            Console.Clear()
+
+
+
+            Do
+                Console.WriteLine("Please enter a nubmer you wish to calculate")
+                Try
+                    firstNumber = Console.ReadLine()
+                    work1 = CDec(firstNumber)
+                    Console.WriteLine("now select an operator:" & vbNewLine _
+                              & "+ press 1" & vbNewLine & "- press 2" & vbNewLine & "* Press 3" & vbNewLine _
+                              & "/ press 4" & vbNewLine & "quit press quit")
+                    operation = Console.ReadLine()
+                    If operation = "q" Then
+
+                        Exit Sub
+                    End If
+
+                    Console.WriteLine("What is the second number you wish to calculate?")
+                    secondNumber = Console.ReadLine
+                    work2 = CDec(secondNumber)
+
+
+
+
+                    badData = False
+                Catch ex As Exception
+                    Console.WriteLine("Numbers entered into calculator must be numbers")
+
+                    badData = True
+                    If firstNumber = "q" Or secondNumber = "q" Then
+                        Exit Sub
+                    End If
+
+                End Try
+            Loop Until badData = False
+
+
+            If badData = False Then
+                If operation = "1" Then
+                    result = work1 + work2
+                    Console.WriteLine(work1 & "+" & work2 & "=" & result)
+                    Console.ReadLine()
+                ElseIf operation = "2" Then
+                    result = work1 - work2
+                    Console.WriteLine(work1 & "-" & work2 & "=" & result)
+                    Console.ReadLine()
+                ElseIf operation = "3" Then
+                    result = work1 * work2
+                    Console.WriteLine(work1 & "*" & work2 & "=" & result)
+                    Console.ReadLine()
+                ElseIf operation = "4" Then
+                    result = work1 / work2
+                    Console.WriteLine(work1 & "/" & work2 & "=" & result)
+                    Console.ReadLine()
+                Else
+                    Console.WriteLine("please select a valid operator value")
+                    Console.ReadLine()
+
+                End If
+
+                Console.ReadLine()
+
+            Else
+            End If
+
+
+
+        Loop
+
+    End Sub
+
+End Module
